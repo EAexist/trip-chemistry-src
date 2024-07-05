40ea2be505b476e5c00c8ebe4ec79a41d6ed5134
Pyo Hyeon <megatwins@kaist.ac.kr>
2024-07-05 15:52:50 +0900

    feat: 팁 아이콘 텍스트(Tip.)로 교체

--- components/KakaoLoginHelp.tsx
+++ components/KakaoLoginHelp.tsx
@@ -1,9 +1,10 @@
-import { Help } from "@mui/icons-material";
 import { Stack } from "@mui/material";
 
 export const KakaoLoginHelp = () => (
     <Stack>
-        <Help fontSize="inherit" />
+        <h2>
+            Tip.
+        </h2>
         <p className="typography-note">
             {
                 "카카오 로그인을 이용하면\n웹 링크를 잃어버려도 테스트 결과를 안전하게 불러올 수 있어요."
